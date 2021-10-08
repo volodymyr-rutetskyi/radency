54e10febb3621c6fb4a68c9d984040cbebe429ec
vladimirochek <volodymyr.rutetskyi@ukr.net>
2021-10-08 11:04:06 +0300

    initialized a project with basic webpack configuration

---
+++ notes app (task 1)/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './index.js',
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }), 
+        new CleanWebpackPlugin()
+    ]
+}
